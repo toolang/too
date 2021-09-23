bfe40f5ea1cd19f72f1e4a894e9be7a254b095a0
brewlin <97404667@qq.com>
2021-09-23 17:04:34 +0800

    [runtime] rm unused extern

--- runtime/syscall/root.s
+++ runtime/syscall/root.s
@@ -1,51 +1,51 @@
 .text
 
-.globl get_sp
-get_sp:
+.globl runtime_gc_get_sp
+runtime_gc_get_sp:
     movq %rsp,%rax
     ret
 
-.globl get_bp
-get_bp:
+.globl runtime_gc_get_bp
+runtime_gc_get_bp:
     movq %rbp,%rax
     ret
 
-.globl get_di
-get_di:
+.globl runtime_gc_get_di
+runtime_gc_get_di:
     movq %rdi,%rax
     ret
 
-.globl get_si
-get_si:
+.globl runtime_gc_get_si
+runtime_gc_get_si:
     movq %rsi,%rax
     ret
 
-.globl get_dx
-get_dx:
+.globl runtime_gc_get_dx
+runtime_gc_get_dx:
     movq %rdx,%rax
     ret
 
-.globl get_cx
-get_cx:
+.globl runtime_gc_get_cx
+runtime_gc_get_cx:
     movq %rcx,%rax
     ret
 
-.globl get_r8
-get_r8:
+.globl runtime_gc_get_r8
+runtime_gc_get_r8:
     movq %r8,%rax
     ret
 
-.globl get_r9
-get_r9:
+.globl runtime_gc_get_r9
+runtime_gc_get_r9:
     movq %r9,%rax
     ret
 
-.globl get_ax
-get_ax:
+.globl runtime_gc_get_ax
+runtime_gc_get_ax:
     movq %rax,%rax
     ret
 
-.globl get_bx
-get_bx:
+.globl runtime_gc_get_bx
+runtime_gc_get_bx:
     movq %rbx,%rax
     ret
