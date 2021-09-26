f1e592285db5b6da65862e1909c908926eb8be77
brewlin <97404667@qq.com>
2021-09-26 12:52:18 +0800

    [asm] opt main call instruct

--- runtime/syscall/asm_amd64.s
+++ runtime/syscall/asm_amd64.s
@@ -6,24 +6,12 @@ main:
     sub $0, %rsp
     push %rsi
     push %rdi
-	mov std_initmalloc@GOTPCREL(%rip), %rax
-	mov %rax,%r10
-	mov $0, %rax
-	call *%r10
-    mov runtime_gc_gc_init@GOTPCREL(%rip), %rax
-    mov %rax, %r10
-    mov $0, %rax
-    call *%r10
+	call std_initmalloc
+	call runtime_gc_gc_init
     pop  %rdi
     pop  %rsi
-    mov runtime_args_init@GOTPCREL(%rip), %rax
-    mov %rax,%r10
-    call *%r10
-
-    mov main_main@GOTPCREL(%rip), %rax
-    mov %rax, %r10
-    mov $0, %rax
-    call *%r10
+	call runtime_args_init
+	call main_main
     mov %rbp, %rsp
     pop %rbp
     ret
