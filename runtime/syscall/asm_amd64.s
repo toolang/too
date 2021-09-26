6347734dbe365e5fdb5e7c2bd97304b9bf895c05
brewlin <97404667@qq.com>
2021-09-26 17:30:05 +0800

    [runtime] fix args init

--- runtime/syscall/asm_amd64.s
+++ runtime/syscall/asm_amd64.s
@@ -1,20 +1,14 @@
 .text
     .global main
 main:
-    push %rbp
-    mov %rsp, %rbp
-    sub $0, %rsp
-    push %rsi
-    push %rdi
 	call std_initmalloc
 	call runtime_gc_gc_init
-    pop  %rdi
-    pop  %rsi
+    mov (%rsp),%rdi
+    lea 16(%rsp),%rsi
 	call runtime_args_init
 	call main_main
-    mov %rbp, %rsp
-    pop %rbp
 	mov $0,%rdi
 	call std_die
-    ret
+    leaveq
+    retq
 	
